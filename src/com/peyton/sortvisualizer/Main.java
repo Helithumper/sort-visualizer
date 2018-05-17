5692af81b7a160f4e7356d28853bc6c2b3a26be9
Helithumper <peyton.duncan45@gmail.com>
2018-05-17 00:42:40 -0400

    Added print to main

--- src/com/peyton/sortvisualizer/Main.java
+++ src/com/peyton/sortvisualizer/Main.java
@@ -3,7 +3,7 @@ package com.peyton.sortvisualizer;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Project Goes Here!");
 
 	}
 
